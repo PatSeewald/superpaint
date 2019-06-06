c132cae804f91d2d0190e2f9b04a5f92ea5742a8
seewpatr <Cr203210>
2019-06-06 09:35:20 +0200

    camera begonnen

--- www/js/app.js
+++ www/js/app.js
@@ -1,91 +1,110 @@
-console.log( 'JS loaded' );
-var superPaint = {
-  color:'#fff',
-  initPens:function() {
-    var colors = ['#000','#f00','#0f0','#00f','#ff0','#f0f','#0ff','#ccc','#fc3','#63c'];
-    for (let i in colors ) {
-      $( '<pen>' )
-        .css({background:colors[i]})
-        .on('click', this.activatePen(colors[i]) )
-        .appendTo( 'footer' );
-    }
-  },
-  activatePen:function( newColor ) {
-    return function() {
-      superPaint.color = newColor;
-      $( '.active' ).removeClass( 'active');
-      $( this ).addClass( 'active' );
-    }
-  },
-  drawModus: 'line', /*dot|line*/
-  dotSize:3,
-  drawStart: function(e) {
-    var offset = $('#stage').offset();
-    superPaint.oldX = e.touches[0].pageX - offset.left;
-    superPaint.oldY = e.touches[0].pageY - offset.top;
-    superPaint.draw(e);
-  },
-  draw: function(e) {
-    var offset = $('#stage').offset();
-    var x = e.touches[0].pageX - offset.left;
-    var y = e.touches[0].pageY - offset.top;
-    var ctx = superPaint.ctx;
-    ctx.fillStyle = ctx.strokeStyle = superPaint.color;
-    ctx.lineWidth = superPaint.dotSize;
-    ctx.beginPath();
-    if ( superPaint.drawModus == 'dot' ) {
-      ctx.arc(x,y,superPaint.dotSize,0,2*Math.PI);
-      ctx.fill();
-    }
-    if ( superPaint.drawModus == 'line' ) {
-      ctx.moveTo(superPaint.oldX,superPaint.oldY);
-      ctx.lineTo(x,y);
-      ctx.stroke();
-      superPaint.oldX = x;
-      superPaint.oldY = y;
-    }
+console.log('JS loaded');
 
-  },
-  initCanvas:function() {
-    this.ctx = $('#stage').get(0).getContext('2d');
-    this.clearAll();
-    $( '#stage' )
-      .attr({
-        width:$(window).width(),
-        height:$(window).height()-80
-      })
-      .on('touchstart', this.drawStart )
-      .on('touchmove', this.draw );
-  },
-  clearAll:function() {
-    var ctx = superPaint.ctx;
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(0,0,$( '#stage' ).width(),$( '#stage' ).height());
-  },
-  saveData:function() {
-    window.canvas2ImagePlugin.saveImageDataToLibrary(
-         function(msg){
-            alert( 'Daten sind gespeichert!' );
-         },
-         function(err){
-            alert( 'Fehler beim Speichern.');
-         },
-         $('#stage').get(0)
-    );
-  },
-  init:function() {
-    this.initPens();
-    this.initCanvas();
-    $('#b_clear').on('click',this.clearAll );
-    $('#b_save').on('click',this.saveData );
-  }
+window.onerror = (err) => {
+  alert(err);
+  return true;
 }
 
+let superPaint = (() => { // IIFE
+  let _t = {
+    color: '#fff',
+    initPens() {
+      let colors = ['#000', '#f00', '#0f0', '#00f', '#ff0', '#f0f', '#0ff', '#ccc', '#fc3', '#63c'];
+      for (let i in colors) {
+        $('<pen>')
+          .css({ background: colors[i] })
+          .on('click', _t.activatePen(colors[i]))
+          .appendTo('footer');
+      }
+    },
+    activatePen(newColor) {
+      return () => {
+        _t.color = newColor;
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+      }
+    },
+    drawModus: 'line', /*dot|line*/
+    dotSize: 3,
+    drawStart(e) {
+      let offset = $('#stage').offset();
+      _t.oldX = e.touches[0].pageX - offset.left;
+      _t.oldY = e.touches[0].pageY - offset.top;
+      _t.draw(e);
+    },
+    draw(e) {
+      let offset = $('#stage').offset();
+      let x = e.touches[0].pageX - offset.left;
+      let y = e.touches[0].pageY - offset.top;
+      let ctx = superPaint.ctx;
+      ctx.fillStyle = ctx.strokeStyle = _t.color;
+      ctx.lineWidth = _t.dotSize;
+      ctx.beginPath();
+      if (_t.drawModus == 'dot') {
+        ctx.arc(x, y, _t.dotSize, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+      if (_t.drawModus == 'line') {
+        ctx.moveTo(_t.oldX, _t.oldY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        _t.oldX = x;
+        _t.oldY = y;
+      }
 
-document.addEventListener( 'deviceready', function() {
-  console.log( 'Device ready' );
-  $( document ).ready( function() {
-    console.log( 'DOM ready, alles ready...' );
+    },
+    initCanvas() {
+      _t.ctx = $('#stage').get(0).getContext('2d');
+      _t.clearAll();
+      $('#stage')
+        .attr({
+          width: $(window).width(),
+          height: $(window).height() - 80
+        })
+        .on('touchstart', _t.drawStart)
+        .on('touchmove', _t.draw);
+    },
+    clearAll() {
+      let ctx = _t.ctx;
+      ctx.fillStyle = '#fff';
+      ctx.fillRect(0, 0, $('#stage').width(), $('#stage').height());
+    },
+    saveData() {
+      window.canvas2ImagePlugin.saveImageDataToLibrary(
+        (msg) => {
+          alert('Daten sind gespeichert!');
+        },
+        (err) => {
+          alert('Fehler beim Speichern.');
+        },
+        $('#stage').get(0)
+      );
+    },
+    addPhoto2Canvas(image) {
+      let bg = new Image();
+      bg.src = image;
+    },
+    openCam(e) {
+      e.preventDefault();
+      navigator.camera.getPicture(_t.addPhoto2Canvas, () => {
+        alert('Camera Fehler');
+      });
+    },
+    init() {
+      _t.initPens();
+      _t.initCanvas();
+      $('#b_foto').on('click', _t.openCam);
+      $('#b_clear').on('click', _t.clearAll);
+      $('#b_save').on('click', _t.saveData);
+    }
+  }
+  return _t;
+}) ();
+
+document.addEventListener('deviceready', () => {
+  console.log('Device ready');
+  $(document).ready(() => {
+    console.log('DOM ready, alles ready...');
     superPaint.init();
 
 
